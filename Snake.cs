76b5eb7482d3d22c8cfa97b67f0aab8a1763b6b9
Kateryna Dementieva <katerynadementieva@gmail.com>
2024-11-30 14:41:10 +0100

    Changed ForegroundColor to green

--- Snake.cs
+++ Snake.cs
@@ -77,7 +77,7 @@ class Program
 
 
 
-            Console.ForegroundColor = ConsoleColor.White;
+            Console.ForegroundColor = ConsoleColor.Green;
 
             for (int i = 0; i < screenwidth; i++)
 
